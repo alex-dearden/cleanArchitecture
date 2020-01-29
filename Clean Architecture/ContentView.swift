d5e514fc48b550814ffe6228524bbed9b1a9842b
alex-dearden <alex.dearden@clearscore.com>
2020-01-29 14:07:24 +0000

    Initial Commit

---
+++ Clean Architecture/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Clean Architecture
+//
+//  Created by Alex Dearden on 29/01/2020.
+//  Copyright © 2020 Alex Dearden. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
